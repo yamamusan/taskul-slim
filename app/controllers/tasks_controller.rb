cdd42342feeba78af1518f65322ab6aec10eca6a
yamamusan <fumito@yamamurafumitonoMacBook-ea.local>
2018-06-28 18:43:55 +0900

    登録・更新メッセージの日本語化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to @task, notice: t('.success', task: Task.model_name.human) }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to @task, notice: t('.success', task: Task.model_name.human) }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
