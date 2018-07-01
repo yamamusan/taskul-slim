df6d81281dbb26f85c3458b7b06597b34f18939b
yamamusan <fumito@yamamurafumitonoMacBook-ea.local>
2018-07-01 22:08:39 +0900

    sort部品を取得するヘルパー追加

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module TasksHelper
+  def sort_items
+    select_hash = []
+    select_hash << ['ID(昇順)', 'id asc']
+    select_hash << ['ID(降順)', 'id desc']
+    select_hash << ['タイトル(昇順)', 'title asc']
+    select_hash << ['タイトル(降順)', 'title desc']
+    select_hash << ['優先度(昇順)', 'priority asc']
+    select_hash << ['優先度(降順)', 'priority desc']
+  end
 end
